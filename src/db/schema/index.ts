ed11d40da85831107d65e6764062c49950e7d8ac
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-15 21:14:24 -0300

    add questions table

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,7 +1,9 @@
 // Criação do barrel file, é um aquivo que exporta todos os aquivos desta pasta.
 
+import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 export const schema = {
     rooms,
+    questions
 }
